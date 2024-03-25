44745420bc752d97915134129ddf51dedcb33654
imadnanhassan <abulhassn.dev@gmail.com>
2024-03-25 23:33:15 +0600

    user model

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,17 @@
-import express, { Application, NextFunction, Request, Response } from "express";
-import cors from "cors";
-const app = express();
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+const app = express()
 
-
-app.use(cors());
+app.use(cors())
 
 // parser
-app.use(express.json());
-app.use;
-express.urlencoded({ extended: true });
+app.use(express.json())
+app.use
+express.urlencoded({ extended: true })
 
 // tasting
-app.get("/", (req: Request, res: Response) => {
-  res.send("Working Sucessfully");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Working Sucessfully')
+})
 
-export default app;
+export default app
