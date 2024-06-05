d74187f0046e3db7c5b9c1257a8074c27c2ee8f8
Dev-Hassan <abulhassan.dev@gmail.com>
2024-06-06 02:40:51 +0600

    Create GenerateUser Utility Function

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,10 +1,17 @@
+import config from "../../../config";
 import { IUser } from "./user.interface";
 import { User } from "./user.model";
+import { generateUserId } from "./user.utils";
 
 const createUser = async (user: IUser) => {
   // auto Id,
+  const id = generateUserId();
+  user.id = id;
 
-  // password
+  // default password
+  if (!user.password) {
+    user.password = config.default_student_pass as string;
+  }
 
   const createUser = await User.create(user);
 
